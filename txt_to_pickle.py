4f21980fa2851613f5de8866869d15f2be81d369
Matéo <136356190+MateoMelki@users.noreply.github.com>
2024-05-27 14:49:35 +0200

    Update and rename csv_to_pickle.py to txt_to_pickle.py

--- txt_to_pickle.py
+++ txt_to_pickle.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Mon May 27 14:24:12 2024
-
-@author: mateo
-"""
 
 import pandas as pd
+
+"""
+To translate file from txt to pkl for faster loading
+txt file of table s7 and s8 can be found at https://www.science.org/doi/10.1126/science.aax1971#supplementary-materials
+from the article https://pubmed.ncbi.nlm.nih.gov/31488706/
+"""
 
 df7=pd.read_table('Biomart_and_Packer_data/NIHMS1604359-supplement-Table_S7.txt')
 df7.to_pickle('Biomart_and_Packer_data/NIHMS1604359-supplement-Table_S7.pkl')
